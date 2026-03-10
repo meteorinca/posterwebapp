c6c3645760f00c6ce288f2804d26b5315ba96bb4
meteorinca <meteorinca@gmail.com>
2026-03-10 14:42:40 -0400

    add img update

--- data.js
+++ data.js
@@ -4,5 +4,6 @@ const POSTERS = [
     "Back to the Future Part II.jpeg",
     "Back to the Future Part III.jpeg",
     "Curb Your Enthusiasm (2000).jpg",
-    "Inception.jpg"
+    "Inception.jpg",
+    "Arrested Development (2003) - Season 1.jpg"
 ];
